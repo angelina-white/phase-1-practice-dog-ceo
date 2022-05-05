da56828a69a1db07c162fee3aba32af2c5d7f435
Angelina <angelinarwhite@gmail.com>
2022-05-04 23:58:48 -0400

    commit tree 0d540e980787746f02b31d14e5dc6c656e77fb79

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', (event) =>
+{
+    getImages();
+    getBreeds();
+})
+
+function getImages()
+{
+    const imgDiv = document.getElementById('dog-image-container')
+    
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((resp) => resp.json())
+    .then((element) =>
+    {
+        element.message.forEach((e) =>
+        {
+            const img = document.createElement('img')
+            img.src = e
+            imgDiv.append(img)
+        })
+    })
+}
+
+function getBreeds()
+{
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then((resp) => resp.json())
+    .then((element) =>
+    {
+        const ul = document.getElementById('dog-breeds')
+        for (const key in element.message) 
+        {
+            const li = document.createElement('li')
+            li.innerText = key
+            li.addEventListener('click', () =>
+            {
+                console.log("I was clicked")
+                li.style.color = "pink"
+            })
+            ul.append(li)
+        }
+    })
+}
